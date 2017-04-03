eefce22472acdfebbc124b7a5e47649ea6556a2e
nicohansen <nicolas.han.sen@hotmail.com>
2017-04-02 22:13:37 -0300

    se agrego la clase empleado

---
+++ Empleado.java
@@ -0,0 +1,32 @@
+
+public class Empleado extends Trabajador {
+
+	private String cuil;
+	private double sueldo;
+	
+	public Empleado(String nombre, int dni, double sueldo) {
+		super(nombre, dni);
+		this.sueldo = sueldo;
+	}
+
+	public String getCuil() {
+		return cuil;
+	}
+
+	public void setCuil(String cuil) {
+		this.cuil = cuil;
+	}
+
+	public double getSueldo() {
+		return sueldo;
+	}
+
+	public void setSueldo(double sueldo) {
+		this.sueldo = sueldo;
+	}
+	
+	
+	
+	
+
+}
